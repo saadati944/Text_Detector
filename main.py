7c63a23490c9c3a493a5193505e8624ef190b4c2
saadati944 <saadati944@gmail.com>
2020-10-19 13:13:03 +0330

    show menu and wait for user input

---
+++ main.py
@@ -0,0 +1,23 @@
+from os import system,name
+
+#clear screen
+def clear():
+    _= system('cls' if name=='nt' else 'clear')
+
+#show menu
+def menu():
+    print('text detector version 0.0')
+    print()
+    print(' e\t:\texit')
+    print(' p\t:\tprocess new text file')
+    print(' a\t:\tadd new file to database')
+    print(' d\t:\tdump database')
+    print(' l\t:\tload database (ignore changes)')
+    print()
+
+while True:
+    clear()
+    menu()
+    ans=input('your choice : ')
+    if ans=='e':
+        exit()
