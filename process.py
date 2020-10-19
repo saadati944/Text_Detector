9757f9bd336f844842da390805bc6d752bef6439
saadati944 <saadati944@gmail.com>
2020-10-19 18:08:51 +0330

    add getscores function in process.py to calculate score of a file

--- process.py
+++ process.py
@@ -1,3 +1,5 @@
+import database
+
 #count words count and return in dict format
 def count(text):
     cache=''
@@ -13,3 +15,25 @@ def count(text):
                 res[cache]=1
             cache =''
     return res
+
+
+def getscores(pth):
+    scores={}
+    with open(pth,'r',encoding='utf-8') as f:
+        res=count(f.read())
+    restotal=0.0
+    for k in res:
+        restotal+=res[k]
+    for category in database.database.keys():
+        score=0.0
+        cat=database.database[category]
+        cattotal=0.0
+        for k in cat:
+            cattotal+=cat[k]
+        for k in res :
+            if k in cat:
+                score+=1-abs((cat[k]/cattotal)-(res[k]/restotal))
+        #show resaults
+        #print(f'\n"{category}"\t:\t{score}\n')
+        scores[category]=score
+    return scores
