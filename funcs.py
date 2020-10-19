69f018073392dc00a4921b814d468cc6e534ab99
saadati944 <saadati944@gmail.com>
2020-10-19 18:07:30 +0330

    add funcs.py to store all useful little functions

---
+++ funcs.py
@@ -0,0 +1,36 @@
+from os import system,name
+import random
+
+__version__='0.1'
+
+#clear screen
+def clear():
+    _= system('cls' if name=='nt' else 'clear')
+
+#show menu
+def menu():
+    print('text detector version',__version__)
+    print()
+    print(' e\t:\texit')                                #v
+    print(' p\t:\tprocess new text file')               #v
+    print(' a\t:\tadd new file to database')            #v
+    print(' d\t:\tdump database')                       #v
+    print(' l\t:\tload database (ignore changes)')      #v
+    print(' w\t:\twipe database')                       #v
+    print()
+
+def help():
+    print('text detector version',__version__)
+    print('\nusage : main.py operand [params]')
+    print('\noperands:')
+    print(' -p FILENAME\t:\tprocess new text file')
+    print(' -a CATEGORY FILENAME\t:\tadd new file to database')
+    print(' -w\t\t:\twipe database\n')
+    print(' -c\t\t:\tshow avaliable categories\n')
+
+def areUsure():
+    rnd=random.random()
+    if input(f"enter '{rnd}' to continue : ")!=str(rnd):
+        print('not equal !!!')
+        return False
+    return True
